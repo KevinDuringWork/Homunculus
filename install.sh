8f4193f272cbc7047debe22259f50d68506b0d72
Kevin Shi <kshi@nygenome.org>
2020-05-06 13:21:09 -0400

    updates

--- install.sh
+++ install.sh
@@ -12,4 +12,4 @@ curl -L ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/release/NA12878_HG001/latest/G
 
 curl -L ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/release/NA12878_HG001/latest/GRCh37/HG001_GRCh37_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-10X-SOLID_CHROM1-X_v.3.3.2_highconf_PGandRTGphasetransfer.vcf.gz.tbi > NA12878.vcf.gz.tbi
 
-print "Installed"
+printf "Installed"
